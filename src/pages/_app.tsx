d72eb41de5928795ba0fed6d0f2cb5f0abd0297f
EggLovej <egglovej@gmail.com>
2025-07-21 17:39:29 +0200

    vercel analytics + speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import "@/styles/globals.css";
-import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
+import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
 import Layout from "@/components/layout/Layout";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Analytics />
+      <SpeedInsights />
+    </>
   );
 }
 
