babb5c723947461560b9d5830eacd385aae9f3ca
EggLovej <egglovej@gmail.com>
2025-08-03 09:45:26 +0200

    added revalidate to sanity call + changed perspective to only include published content

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -3,6 +3,7 @@ import { createClient } from "@sanity/client";
 export const client = createClient({
   projectId: "2mgbz7vp",
   dataset: "production",
-  apiVersion: "2023-01-01", // stable, supported version
-  useCdn: false, // ensures querying via API not CDN
+  apiVersion: "2023-01-01",
+  useCdn: false,
+  perspective: "published", 
 });
