ba58d7d53f010a9ff92099db15b8c65578ab87c3
EggLovej <egglovej@gmail.com>
2025-08-03 10:38:42 +0200

    linting

--- src/lib/getStaticProps.ts
+++ src/lib/getStaticProps.ts
@@ -1,16 +1,10 @@
 import { GetStaticPropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 import nextI18NextConfig from "../../next-i18next.config.js";
 
-export async function getI18nStaticProps(
-  context: GetStaticPropsContext,
-  namespaces = ["common"]
-) {
+export async function getI18nStaticProps(context: GetStaticPropsContext, namespaces = ["common"]) {
   const locale = context.locale || "de";
-  const i18nProps = await serverSideTranslations(
-    locale,
-    namespaces,
-    nextI18NextConfig
-  );
+  const i18nProps = await serverSideTranslations(locale, namespaces, nextI18NextConfig);
   return { props: { ...i18nProps } };
 }
--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -5,5 +5,5 @@ export const client = createClient({
   dataset: "production",
   apiVersion: "2023-01-01",
   useCdn: false,
-  perspective: "published", 
+  perspective: "published",
 });
